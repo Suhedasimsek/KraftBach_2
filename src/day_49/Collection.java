0f54d9e63dd8d12064fe64eb512a56e9c81c9196
Suhedasimsek <114024071+Suhedasimsek@users.noreply.github.com>
2022-09-28 20:44:03 +0300

    First

---
+++ src/day_49/Collection.java
@@ -0,0 +1,15 @@
+package day_49;
+
+public class Collection {
+    //Kullacıdan 5 veri al toplu yazdır
+    public static void main(String[] args) {
+
+    }
+    public static veriAlEkranaYazdır (){
+
+
+
+    }
+
+
+}
